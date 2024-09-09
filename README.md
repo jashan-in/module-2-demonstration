30c9dc3866c2cae0d164fb4d87f7ebb771d240ee
Jashanpreet Singh <jashanpreetsingh11@rrc.ca>
2024-09-09 10:06:45 -0500

    Set up file directories for module 02

---
+++ README.md
@@ -0,0 +1,7 @@
+# Module 02 Demonstration
+
+## Demonstration
+Introduction to Python: Documentation, Variables, Printinng
+
+## Author 
+Jashanpreet Singh
---
+++ module_2.py
@@ -0,0 +1,13 @@
+"""
+Description: Module 02 demonstration
+Author: Jashanpreet Singh
+Date: September 09, 2024
+Usage: To demonstrate content from Module 02.
+"""
+
+# This is a single line comment
+
+"""
+This is a multi line comment
+it can span over multiple lines
+"""
